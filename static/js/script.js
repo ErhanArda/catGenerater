970b33751dd4d570d9e632e16845924ed79c51ff
Erhan <erhanarda7@gmail.com>
2019-12-13 01:46:37 +0300

    cat generater api usage

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+/* 
+ function reset(){
+    document.getElementById('result').remove();
+
+ }*/
+
+ function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&size=medium';
+    div.appendChild(image);
+ }
+
+ var generate = false;
+
+ document.getElementById("catGenerator").onclick = function(){
+    if(generate == true){
+       location.reload();
+    }
+    else{
+       generate =  true;
+       document.getElementById("catGenerator").innerHTML = "Reset Generate";
+       generateCat();
+    }
+
+ }
+
