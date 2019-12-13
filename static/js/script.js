c7c72f49f086fd80c09515542c42f5d37b4a541a
Erhan <erhanarda7@gmail.com>
2019-12-13 11:47:25 +0300

    gif size: small

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,7 @@
  function generateCat(){
     var image = document.createElement('img');
     var div = document.getElementById('flex-cat-gen');
-    image.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&size=medium';
+    image.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&size=small';
     div.appendChild(image);
  }
 
