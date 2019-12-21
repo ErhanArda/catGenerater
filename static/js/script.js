5be43250d1f03d78fa08711e8de1349f2f741fa4
Erhan <erhanarda7@gmail.com>
2019-12-21 16:26:25 +0300

    generate radio button

--- static/js/script.js
+++ static/js/script.js
@@ -7,13 +7,13 @@
  function generateCat(){
     var image = document.createElement('img');
     var div = document.getElementById('flex-cat-gen');
-    
-    image.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&random='+Math.floor(Math.random() * 999999999) + 1;
-    
-    document.getElementById("catGenerator").disabled = true; 
+     image.src = 'http://thecatapi.com/api/images/get?format=src&type=gif&random='+Math.floor(Math.random() * 999999999) + 1;
+
+     document.getElementById("catGenerator").disabled = true; 
     document.getElementById("catGenerator").classList.add("btn-danger");
     image.style.width = "500px";
     image.style.height = "333px";
+  
     if(screen.width<=768){
       image.style.width = "320";
       image.style.height = "180px";
@@ -29,9 +29,21 @@
  }
 
  window.addEventListener('load', 
+ 
  function() { 
    generateCat();
  }, false);
+
+ const checkbox = document.getElementById('checkbox')
+ var refreshIntervalId = null;
+
+ checkbox.addEventListener('change', (event) => {
+   if (event.target.checked) {
+      refreshIntervalId =  setInterval(generateCat, 4000);
+   } else {
+
+       clearInterval(refreshIntervalId);   }
+ })
 
  document.getElementById("catGenerator").onclick = function(){
     
